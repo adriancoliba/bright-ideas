8778893a578244fd9cd3896d910da6b0cfc9d2da
adriancoliba <adrian.coliba5@gmail.com>
2019-11-05 01:34:27 +0200

    Fetching all notes from firebase.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import './App.css';
+
+const firebase = require('firebase');
 
 class App extends React.PureComponent {
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteId: '',
+      selectedNote: {},
+      notesAll: [],
+    }
+  }
+
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection('notesAll')
+      .onSnapshot(serverUpdate => {
+        const notesAll = serverUpdate.docs.map(doc => {
+          const data = doc.data();
+          data.id = doc.id;
+          return data;
+        });
+        console.log(notesAll);
+        this.setState({notesAll})
+      })
+  }
+
   render(){
+    const { notesAll } = this.state;
+
+    const displayNotesAll = notesAll && notesAll.map(note => {
+      return (
+        <div key={note.id}>
+          <h3>title: {note.title}</h3>
+          <p>body: {note.body}</p>
+        </div>
+      )
+    });
+
     return (
-        <div>This is still a test</div>
+      <div>{displayNotesAll}</div>
     )
   }
 }
