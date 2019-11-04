11fcc6e05728228362b372bec06fb8b49525eed6
adriancoliba <adrian.coliba5@gmail.com>
2019-11-05 00:41:51 +0200

    Added firebase dependency; configured for development.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render(){
+    return (
+        <div>This is still a test</div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import App from './App'
 
-ReactDOM.render(<div>This is a test</div>, document.getElementById('root'));
+const firebase = require('firebase');
+require('firebase/firestore');
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyALTvRAm-tUvTGE5rDWOYs2bLfArDW8WcU",
+    authDomain: "note-taking-app-18952.firebaseapp.com",
+    databaseURL: "https://note-taking-app-18952.firebaseio.com",
+    projectId: "note-taking-app-18952",
+    storageBucket: "note-taking-app-18952.appspot.com",
+    messagingSenderId: "547358686113",
+    appId: "1:547358686113:web:0b92acfceb87816f2329e0",
+    measurementId: "G-J0W3WN4WXP"
+};
+// Firebase Initialization
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 serviceWorker.unregister();
